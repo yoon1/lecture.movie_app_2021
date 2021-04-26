4d0a8614ff2761d616bbb71ba4880dc5f8f6ea7d
DESKTOP-ICP797S\USER <gotslaekfsla7@gmail.com>
2021-04-26 15:01:41 +0900

    최종

---
+++ src/routes/Detail.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class Detail extends Component {
+  componentDidMount() {
+    const { location, history } = this.props;
+    if (location.state === undefined) {
+      history.push("/");
+    }
+  }
+
+  render() {
+    const { location } = this.props;
+    if (location.state) {
+      return <span>{location.state.title}</span>;
+    } else {
+      return null;
+    }
+    // return <span>{location.state.title}</span>;
+  }
+}
+
+export default Detail;
